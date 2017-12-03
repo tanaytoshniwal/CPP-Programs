792f99a65e8565eec6a1ebdd5e4b2c420a436ae3
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-12-04 01:14:30 +0530

    enum with init

---
+++ enum2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+enum basicneeds {laptop=69, coffee=6969, sleep=696969};
+int main() {
+  basicneeds needs;
+  needs=laptop;
+  cout << needs<<"\n";
+  return 0;
+}
